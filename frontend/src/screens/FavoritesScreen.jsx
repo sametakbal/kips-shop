ab0214acfa620e62844ec9fc66ef337389353b79
Samet Akbal <sametakbal7@gmail.com>
2023-07-01 00:45:20 +0300

    Fix #6: Favorites screen

---
+++ frontend/src/screens/FavoritesScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavoritesScreen = () => {
+    return (
+        <div>Favourites Screen</div>
+    )
+}
+
+export default FavoritesScreen
---
+++ frontend/src/screens/FavouritesScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavouritesScreen = () => {
+    return (
+        <div>FavouritesScreen</div>
+    )
+}
+
+export default FavouritesScreen
