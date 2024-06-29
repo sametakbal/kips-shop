ee58041194225bd073d9a1547d17ca93f7711f3e
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2024-06-29 23:39:53 +0300

    product service initial commit

---
+++ product-api/src/main/java/com/kips/product/api/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.kips.product.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
---
+++ product-api/src/test/java/com/kips/product/api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kips.product.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
