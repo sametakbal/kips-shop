e403c4b90eeb09c75d56a1a8807a47160a05a327
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2025-04-03 00:38:56 +0300

    KipsBeApplicationTests mock test for analyse

--- src/test/java/com/akbal/kips/be/KipsBeApplicationTests.java
+++ src/test/java/com/akbal/kips/be/KipsBeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.akbal.kips.be;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class KipsBeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a = 5;
+		Assertions.assertEquals(2+3, a);
 	}
 
 }
